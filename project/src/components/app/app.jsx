3b31db79c269129c95db15f79ab34a6f24b24e68
TSergey123 <46027800+TSergey123@users.noreply.github.com>
2021-06-10 18:14:04 +0300

    module2-task1 done

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import MainScreen from '../main-screen/main-screen';
-import { CARDS_COUNT } from '../../const/const';
+import SignInScreen from '../sign-in/sign-in';
+import AddReviewScreen from '../add-review/add-review';
+import FilmScreen from '../film/film';
+import PlayerScreen from '../player/player';
+import MyListScreen from '../my-list/my-list';
+import NotFoundScreen from '../my-list/my-list';
+import { AppRoute, CARDS_COUNT } from '../../const/const';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const CardData = {
   GENRE: 'Genre',
@@ -10,12 +17,43 @@ const CardData = {
 
 function App() {
   return (
-    <MainScreen
-      cardsCount = {CARDS_COUNT}
-      genre = {CardData.GENRE}
-      date = {CardData.DATE}
-      name = {CardData.NAME}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainScreen
+            cardsCount={CARDS_COUNT}
+            genre={CardData.GENRE}
+            date={CardData.DATE}
+            name={CardData.NAME}
+          />
+        </Route>
+
+        <Route exact path={AppRoute.SIGN_IN}>
+          <SignInScreen />
+        </Route>
+
+        <Route exact path={AppRoute.MY_LIST}>
+          <MyListScreen />
+        </Route>
+
+        <Route exact path={AppRoute.FILM}>
+          <FilmScreen />
+        </Route>
+
+        <Route exact path={AppRoute.ADD_REVIEW}>
+          <AddReviewScreen />
+        </Route>
+
+        <Route exact path={AppRoute.PlAYER}>
+          <PlayerScreen />
+        </Route>
+
+        <Route>
+          <NotFoundScreen />
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- project/src/const/const.js
+++ project/src/const/const.js
@@ -1,1 +1,10 @@
 export const CARDS_COUNT = 29;
+
+export const AppRoute = {
+  MAIN: '/',
+  SIGN_IN: '/login',
+  MY_LIST: '/mylist',
+  FILM: '/films/:id',
+  ADD_REVIEW: '/films/:id/review',
+  PlAYER: '/player/:id',
+};
