4783352dca893e3b42120bbea41b0780ed877d83
TSergey123 <46027800+TSergey123@users.noreply.github.com>
2021-06-11 17:30:44 +0300

    ну вообщем я дурачок

--- project/src/components/film-card/film-card.jsx
+++ project/src/components/film-card/film-card.jsx
@@ -6,7 +6,7 @@ function FilmCard(props) {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src={poster} alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+        <img src={poster} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">{name}</a>
